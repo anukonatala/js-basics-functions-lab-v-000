2136511631801cab1dd8fb835b60974a20fc01b8
Anusha Konatala <anu.konatala@gmail.com>
2018-07-16 07:06:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ function calculatesFarePrice(start, destination){
   let distance = distanceTravelledInFeet(start,destination);
   if (distance <= 400)
   return 0;
-  else if((distance - 400) > 400 && (distance - 400) < 2000)
-  return distance * 0.02;
+  else if(distance > 400 && distance< 2000)
+  return (distance - 400) * 0.02;
   else if(distance > 2000 && distance < 2500)
   return 25;
   else
