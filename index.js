b1ceff5ea3093338568aaf1a5870c32bbca0bc88
Anusha Konatala <anu.konatala@gmail.com>
2018-07-16 06:55:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function calculatesFarePrice(start, destination){
   return 0;
   else if(distance > 400 && distance < 2000)
   return distance * 0.02;
-  else
+  else if(distance > 2000 && distance < 2500)
   return 
   
 }
