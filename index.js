ada030dab711ef2aed529aa7c912a7b500d65300
Anusha Konatala <anu.konatala@gmail.com>
2018-07-16 06:52:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,18 @@ function distanceFromHqInFeet(blocks){
   return distanceFromHqInBlocks(blocks) * 264;
 }
 
-function distanceTravelledInFeet(blocks){
-  return distanceFromHqInFeet(blocks)
+function distanceTravelledInFeet(north,south){
+  return distanceFromHqInFeet(north) -  distanceFromHqInFeet(south);
 }
+
+
+      expect(distanceTravelledInFeet(43, 48)).to.equal(1320);
+    });
+
+    it('returns a distance in feet', function() {
+      expect(distanceTravelledInFeet(50, 60)).to.equal(2640);
+    });
+
+    it('returns distance when destination is below distance', function() {
+      expect(distanceTravelledInFeet(34, 28)).to.equal(1584);
+   
