f7a254cab9298cff3c4129fd3fc8bbfa38282418
Anusha Konatala <anu.konatala@gmail.com>
2018-07-16 06:52:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,13 +12,4 @@ function distanceTravelledInFeet(north,south){
 }
 
 
-      expect(distanceTravelledInFeet(43, 48)).to.equal(1320);
-    });
-
-    it('returns a distance in feet', function() {
-      expect(distanceTravelledInFeet(50, 60)).to.equal(2640);
-    });
-
-    it('returns distance when destination is below distance', function() {
-      expect(distanceTravelledInFeet(34, 28)).to.equal(1584);
-   
+     
